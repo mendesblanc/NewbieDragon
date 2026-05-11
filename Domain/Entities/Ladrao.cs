2addebce8b486b6d4421d2a958525618ac7fe632
RenataBataier <bataierrenata@gmail.com>
2026-05-10 21:14:42 -0300

    Classes filhas 2

--- Domain/Entities/Ladrao.cs
+++ Domain/Entities/Ladrao.cs
@@ -1,9 +1,23 @@
 namespace NewbieDragon.Domain.Entities;
 
-class Ladrao : Personagem
+public class Ladrao : Personagem
 {
+    public Ladrao()
+    {
+        CA = 14;
+        JogadaDeProtecao = 6;
+        Habilidades = new List<string>
+        {
+            "Ataque Furtivo: causa dano extra ao atacar um inimigo desprevenido.",
+            "Talentos de Ladrão: habilidades especiais como furtar bolsos e abrir fechaduras.",
+            "Ouvir Ruídos: consegue detectar sons suspeitos em portas e paredes."
+        };
+    }
+
     public override int CalcularPV()
     {
-        return 7; // Só teste
+        Random dado = new Random();
+        int rolagem = dado.Next(1, 7);
+        return Math.Max(1, rolagem + Constituicao.CalcularModificador());
     }
 }
