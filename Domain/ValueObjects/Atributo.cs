7e6d3f896d4979e63a3423ddfea41f40796f8c6e
Tiago Algauer Vieira <tiagoalgauer@gmail.com>
2026-05-10 21:22:24 -0300

    adiciona Atributo e refatora Personagem

---
+++ Domain/ValueObjects/Atributo.cs
@@ -0,0 +1,31 @@
+namespace NewbieDragon.Domain.ValueObjects;
+
+public class Atributo
+{
+    public string Nome;
+    public int Valor;
+
+    public Atributo(string nome, int valor)
+    {
+        Nome = nome;
+        // Impede valores menores que 3 ou maiores que 18
+        Valor = Math.Clamp(valor, 3, 18);
+    }
+
+    public int CalcularModificador()
+    {
+        if (Valor == 3)  return -3;
+        if (Valor <= 5)  return -2;
+        if (Valor <= 8)  return -1;
+        if (Valor <= 12) return  0;
+        if (Valor <= 15) return +1;
+        if (Valor <= 17) return +2;
+        return                  +3;
+    }
+
+    public override string ToString()
+    {
+        string sinal = CalcularModificador() >= 0 ? "+" : "";
+        return $"{Nome}: {Valor} ({sinal}{CalcularModificador()})";
+    }
+}
